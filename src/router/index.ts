c85e129d3d60a5a9889934fefb11570031af1620
Joralf Quist <joralf.quist@infi.nl>
2022-02-14 15:44:47 +0100

    Example with Vee Validate 4.5.0

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Component from "@/views/Component.vue";
 import VeeValidateField from "@/views/VeeValidateField.vue";
 import VeeValidateForm from "@/views/VeeValidateForm.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    component: Component,
-  },
-  {
-    path: "/404",
-    component: {
-      template: `404`,
-    },
-  },
-  {
-    path: "/posts/:id",
-    component: {
-      template: `posts`,
-    },
-  },
   {
     path: "/vee-validate-field",
     component: VeeValidateField,
